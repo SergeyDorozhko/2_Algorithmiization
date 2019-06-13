b43ab4c0aa5bcb63d00c1b27c1d369959e2bb8aa
SergeyDorozhko <sergdorozhko@gmail.com>
2019-06-13 22:56:41 +0300

    update

---
+++ src/by/epam/my_study/decomposition/Decomposition1.java
@@ -0,0 +1,47 @@
+package by.epam.my_study.decomposition;
+
+import java.util.Scanner;
+
+public class Decomposition1 {
+/*
+Написать метод (методы) для нахождения НОК и НОД двух натуральных чисел
+ */
+
+
+    public int greatestCommonDivisor(int firstNumber, int secondNumber){
+        int minOfTwoNumbers;
+        if(firstNumber < secondNumber){
+            minOfTwoNumbers = firstNumber;
+        }else{
+            minOfTwoNumbers = secondNumber;
+        }
+
+        int greatestCommonDivisor = -1;
+        for(int i = 1; i < minOfTwoNumbers; i++){
+            if(firstNumber % i == 0 && secondNumber % i == 0){
+                greatestCommonDivisor = i;
+
+            }
+        }
+        return greatestCommonDivisor;
+    }
+
+    public int leastCommonMultiple(int firstNumber, int secondNumber,int greatestCommonDivisor){
+        int leastCommonMultiple = firstNumber * secondNumber / greatestCommonDivisor;
+        return leastCommonMultiple;
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input first number: ");
+        int firstNum = scan.nextInt();
+        System.out.println("Input second number: ");
+        int secondNum = scan.nextInt();
+
+        Decomposition1 lCM = new Decomposition1();
+        int greatestCommonDivisor = lCM.greatestCommonDivisor(firstNum, secondNum);
+        int leastCommonMultiple = lCM.leastCommonMultiple(firstNum, secondNum, greatestCommonDivisor);
+        System.out.println("Least common multiple of " + firstNum + " and " + secondNum + " is " + leastCommonMultiple + ".\nGeatest common divisor of this one is " + greatestCommonDivisor);
+
+    }
+}
---
+++ src/by/epam/my_study/decomposition/Decomposition2.java
@@ -0,0 +1,37 @@
+package by.epam.my_study.decomposition;
+
+import java.util.Scanner;
+
+public class Decomposition2 {
+    /*
+    написать метод  для нахождения НОД четырех натуральных чисел
+     */
+    public static int greatestCommonDivisor(int numberOne, int numberTwo, int numberThree, int numberFour ){
+        int minOfNumbers = Math.min(Math.min(numberOne, numberTwo), Math.min(numberThree, numberFour));
+        int gCD = 0;
+        for(int i = 1; i <= minOfNumbers; i++ ){
+            if(numberOne % i == 0 && numberTwo % i == 0 && numberThree % i == 0 && numberFour % i == 0){
+                gCD = i;
+            }
+        }
+        return gCD;
+    }
+
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input first number:");
+        int one = scan.nextInt();
+        System.out.println("Input second number: ");
+        int two = scan.nextInt();
+        System.out.println("Input third number: ");
+        int three = scan.nextInt();
+        System.out.println("Input fourth number: ");
+        int four = scan.nextInt();
+
+
+        System.out.println("Greatest common divisor of this numbers is " + greatestCommonDivisor(one, two, three, four));
+
+
+    }
+}
